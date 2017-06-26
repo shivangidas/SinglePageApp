9983240b14119965b4a3c4e87a293ee78d89dd1b
Shivangi Das <shivangidas@users.noreply.github.com>
2017-06-26 16:19:05 +0530

    Bug fixed
    in smooth scrolling

--- common.js
+++ common.js
@@ -12,7 +12,25 @@ $(document).ready(function () {
 	$('.myCard').hover(function(){
     	$(this).toggleClass('flipped');
   	});
-	$('a[href*=#]:not([href=#])').click(function() {
+	//Smooth scrolling
+	$('a[href*="#"]')
+	  // Remove links that don't actually link to anything
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
 		|| location.hostname == this.hostname) {
 
