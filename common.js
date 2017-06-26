5a8f773f5cd8eca4f5175ed5ebea2a740d2ac70a
Shivangi Das <shivangidas@users.noreply.github.com>
2017-06-26 16:11:39 +0530

    Added smooth scrolling

--- common.js
+++ common.js
@@ -12,4 +12,18 @@ $(document).ready(function () {
 	$('.myCard').hover(function(){
     	$(this).toggleClass('flipped');
   	});
+	$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+		|| location.hostname == this.hostname) {
+
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		   if (target.length) {
+		     $('html,body').animate({
+			 scrollTop: target.offset().top
+		    }, 1000);
+		    return false;
+		}
+	    }
+	});
 });
