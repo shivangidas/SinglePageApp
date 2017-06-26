14892119b47c07a3ba1d488c1ced94b5d70e350d
Shivangi Das <shivangidas@users.noreply.github.com>
2017-06-26 16:19:40 +0530

    Update common.js

--- common.js
+++ common.js
@@ -18,19 +18,6 @@ $(document).ready(function () {
 	  .not('[href="#"]')
 	  .not('[href="#0"]')
 	  .click(function(event) {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
-});
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
 		|| location.hostname == this.hostname) {
 
@@ -44,4 +31,5 @@ $(document).ready(function () {
 		}
 	    }
 	});
+	   
 });
